7c90f54724310fee5bb3f7b9a8724b73a1c0aff7
Arinda Joel <arindajoel24@icloud.com>
2026-04-27 20:34:03 +0300

    Problem Set 2

---
+++ Problem Set 2/nutrition.py
@@ -0,0 +1,29 @@
+fruit = input("Enter fruit: ").title()
+
+d = { 
+    "Apple": 130,
+    "Avocado": 50, 
+    "Banana": 110,
+    "Cantaloupe": 50,
+    "Grapefruit": 60,
+    "Grapes": 90,
+    "Honeydew Melon": 50,
+    "Kiwifruit": 90,
+    "Lemon": 15,
+    "Lime": 20,
+    "Nectarine": 60,
+    "Orange": 80,
+    "Peach": 60,
+    "Pear": 100,
+    "Pineapple": 50,
+    "Plums": 70,
+    "Strawberries": 50,
+    "Sweet Cherries": 50,
+    "Tangerine": 50,
+    "Watermelon": 80
+}
+
+if fruit in d:
+     print(f"Calories: {d[fruit]}")
+else:
+     print("Invalid Input")    
